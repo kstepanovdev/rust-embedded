06b4db446906a513244833fc232125c283be94d9
Kyrylo Stepanov <k.stepanov@tutanota.com>
2024-08-19 23:08:55 +0300

    add rtt debug && change building options

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,18 @@
 #![no_main]
 
 use cortex_m_rt::entry;
-extern crate panic_halt;
+use panic_halt as _;
+use rtt_target::{rprintln, rtt_init_print};
 
 #[entry]
 fn main() -> ! {
-    loop {}
+    rtt_init_print!();
+    let mut x = 0;
+    loop {
+        x += 1;
+        rprintln!("X: {}", x);
+        for _ in 0..1_000_00 {
+            cortex_m::asm::nop();
+        }
+    }
 }
