9e79936741a7c7ae754383a8c0e85ea3a8ee289a
Kyrylo Stepanov <k.stepanov@tutanota.com>
2024-08-21 13:42:46 +0300

    led roulette

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,52 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use embedded_hal::digital::OutputPin;
-use microbit::board::Board;
+use microbit::{board::Board, display::blocking::Display, hal::Timer};
 use panic_halt as _;
+
+const PIXELS: [(u8, u8); 16] = [
+    (0, 0),
+    (0, 1),
+    (0, 2),
+    (0, 3),
+    (0, 4),
+    (1, 4),
+    (2, 4),
+    (3, 4),
+    (4, 4),
+    (4, 3),
+    (4, 2),
+    (4, 1),
+    (4, 0),
+    (3, 0),
+    (2, 0),
+    (1, 0),
+];
 
 #[entry]
 fn main() -> ! {
-    let mut board = Board::take().unwrap();
+    let board = Board::take().unwrap();
 
-    board.display_pins.col1.set_low().unwrap();
-    board.display_pins.row1.set_high().unwrap();
+    let mut timer = Timer::new(board.TIMER0);
+    let mut display = Display::new(board.display_pins);
 
-    loop {}
+    let mut grid = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+    ];
+
+    loop {
+        for (x, y) in PIXELS.iter() {
+            toggle(&mut grid[*y as usize][*x as usize]);
+            display.show(&mut timer, grid, 200);
+            toggle(&mut grid[*y as usize][*x as usize]);
+        }
+    }
+}
+
+fn toggle(led: &mut u8) {
+    *led = if *led == 0 { 1 } else { 0 };
 }
