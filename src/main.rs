b4b25b14a27f83650cd1df9923201ab8b4d6e1e7
Kyrylo Stepanov <k.stepanov@tutanota.com>
2024-08-21 13:21:10 +0300

    setup for microbit

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,18 @@
-#![no_std]
+#![deny(unsafe_code)]
 #![no_main]
-
-use hal::{
-    pac::{self},
-    prelude::*,
-    rcc::RccExt,
-};
-use stm32f4xx_hal as hal;
+#![no_std]
 
 use cortex_m_rt::entry;
+use embedded_hal::digital::OutputPin;
+use microbit::board::Board;
 use panic_halt as _;
-use rtt_target::rtt_init_print;
 
 #[entry]
 fn main() -> ! {
-    let dp = pac::Peripherals::take().unwrap();
-    let rcc = dp.RCC.constrain();
-    let _clocks = rcc.cfgr.freeze();
+    let mut board = Board::take().unwrap();
 
-    let gpiod = dp.GPIOD.split();
+    board.display_pins.col1.set_low().unwrap();
+    board.display_pins.row1.set_high().unwrap();
 
-    let mut green_led = gpiod.pd12.into_push_pull_output();
-    let mut orange_led = gpiod.pd13.into_push_pull_output();
-    let mut red_led = gpiod.pd14.into_push_pull_output();
-    let mut blue_led = gpiod.pd15.into_push_pull_output();
-
-    rtt_init_print!();
-    loop {
-        green_led.set_high();
-        for _ in 0..100_000 {
-            cortex_m::asm::nop();
-        }
-        green_led.set_low();
-
-        orange_led.set_high();
-        for _ in 0..100_000 {
-            cortex_m::asm::nop();
-        }
-        orange_led.set_low();
-
-        green_led.set_high();
-        for _ in 0..100_000 {
-            cortex_m::asm::nop();
-        }
-        green_led.set_low();
-
-        red_led.set_high();
-        for _ in 0..100_000 {
-            cortex_m::asm::nop();
-        }
-        red_led.set_low();
-
-        blue_led.set_high();
-        for _ in 0..100_000 {
-            cortex_m::asm::nop();
-        }
-        blue_led.set_low();
-    }
+    loop {}
 }
