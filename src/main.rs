030503db5e8760bd332168f3e5d705d35ef929e9
Kyrylo Stepanov <k.stepanov@tutanota.com>
2024-08-24 17:01:31 +0300

    microbit i2c accelerometer

--- src/main.rs
+++ src/main.rs
@@ -2,50 +2,63 @@
 #![no_main]
 #![no_std]
 
-use core::fmt::Write;
-
 use cortex_m_rt::entry;
-use heapless::Vec;
+use embedded_hal::i2c::I2c;
+use lsm303agr::{AccelMode, AccelOutputDataRate, Lsm303agr};
 use microbit::{
     board::Board,
-    hal::{
-        uarte::{Baudrate, Parity},
-        Uarte,
-    },
+    hal::{Delay, Twim},
+    pac::twim0::frequency::FREQUENCY_A,
 };
 use panic_halt as _;
-use rtt_target::rtt_init_print;
+use rtt_target::{rprintln, rtt_init_print};
+
+const ACCELEROMETER_ADDR: u8 = 0b0011001;
+const MAGNETOMETER_ADDR: u8 = 0b0011110;
+
+const ACCELEROMETER_ID_REG: u8 = 0x0f;
+const MAGNETOMETER_ID_REG: u8 = 0x4f;
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
     let board = Board::take().unwrap();
 
-    let mut serial = Uarte::new(
-        board.UARTE0,
-        board.uart.into(),
-        Parity::EXCLUDED,
-        Baudrate::BAUD115200,
-    );
-    let mut message_buf: Vec<char, 8> = Vec::new();
-    let mut rx_buf = [0; 1];
+    let mut i2c = Twim::new(board.TWIM0, board.i2c_internal.into(), FREQUENCY_A::K100);
+    let mut acc = [0];
+    let mut mag = [0];
+
+    if i2c
+        .write_read(ACCELEROMETER_ADDR, &[ACCELEROMETER_ID_REG], &mut acc)
+        .is_ok()
+    {
+        rprintln!("Accelerometer ID: {:#b}", acc[0]);
+    } else {
+        rprintln!("Error reading accelerometer ID");
+    }
+
+    if i2c
+        .write_read(MAGNETOMETER_ADDR, &[MAGNETOMETER_ID_REG], &mut mag)
+        .is_ok()
+    {
+        rprintln!("Magnetometer ID: {:#b}", mag[0]);
+    } else {
+        rprintln!("Error reading Magnetometer ID");
+    }
+
+    let mut sensor = Lsm303agr::new_with_i2c(i2c);
+    sensor.init().unwrap();
+    let mut delay = Delay::new(board.SYST);
+
+    sensor
+        .set_accel_mode_and_odr(&mut delay, AccelMode::Normal, AccelOutputDataRate::Hz50)
+        .unwrap();
 
     loop {
-        serial.read(&mut rx_buf).unwrap();
+        if sensor.accel_status().unwrap().xyz_new_data() {
+            let acceleration = sensor.acceleration().unwrap();
 
-        if rx_buf[0] as char == '\r' {
-            for c in message_buf.iter().chain(&['\n', '\r']) {
-                serial.write_char(*c).unwrap();
-            }
-            message_buf.clear();
-        } else {
-            if message_buf.len() == 8 {
-                serial
-                    .write_str("MAX BUF LEN REACHED, PRESS ENTER \r\n")
-                    .unwrap();
-            } else {
-                message_buf.push(rx_buf[0] as char).unwrap();
-            }
+            rprintln!("Acceleration: {:?}", acceleration);
         }
     }
 }
